724a3be6adc6109ff9ec1bb3480c3d4a03dede24
Samuel Senerwa <samuelsenerwa@gmail.com>
2023-05-28 04:21:38 -0700

    fixing checker

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
 """
-Module contains status endpoint
+index
 """
-
+from flask import jsonify, Blueprint
 from api.v1.views import app_views
-from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-@app_views.route("/status")
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
-    """
-    Endpoint to get API status
-    """
-    return jsonify({"status": "OK"})
+    """ checking the status of route """
+    return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats')
-def storage_counts():
-    """return the count of all the classes contained in models
-    """
-    cls_counts = {
-            "amenities": storage.count("Amenity"),
-            "cities": storage.count("City"),
-            "places": storage.count("Place"),
-            "reviews": storage.count("Review"),
-            "states": storage.count("State"),
-            "users": storage.count("Place")
-            }
-    return jsonify(cls_counts)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_of_objects():
+    """ Retrieves the number of each objects by type """
+    objects = {"amenities": 'Amenity', "cities": 'City', "places": 'Place',
+               "reviews": 'Review', "states": 'State', "users": 'User'}
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
